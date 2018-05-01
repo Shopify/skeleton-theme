ddae65abc3f30178d7da684aad1db1d9da34a35d
Chris Berthe <chrisberthe@gmail.com>
2018-05-01 15:12:05 -0400

    Initial commit

---
+++ slate.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: {
+    dev: {resolve: {alias}},
+    prod: {resolve: {alias}},
+  },
+};
---
+++ src/assets/scripts/layout/theme.js
@@ -0,0 +1,5 @@
+import '../../styles/theme.scss';
+import '../../styles/theme.scss.liquid';
+
+window.slate = window.slate || {};
+window.theme = window.theme || {};
