66932130dbbba5e0492dd29f20ed0ad07037f6c1
Sh3rry-W4Z-H3R3 <sherjeelhussain9927@hotmail.com>
2025-06-28 01:09:45 +0100

    lots of wee ones but this is ready to play with

--- andras-garden-heaven/src/components/ui/aspect-ratio.jsx
+++
@@ -1,9 +0,0 @@
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
-
-function AspectRatio({
-  ...props
-}) {
-  return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />;
-}
-
-export { AspectRatio }
